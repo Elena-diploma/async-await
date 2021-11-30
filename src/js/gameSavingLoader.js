8cd15f6a7769a2a3605bd4efb3d533848cd4322f
Elena Tursunova <elenatursunova@gmail.com>
2021-11-30 21:40:03 +0300

    исправления 2

--- src/js/gameSavingLoader.js
+++ src/js/gameSavingLoader.js
@@ -1,10 +1,19 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './gameSaving';
+import UserInfo from './userInfo';
 
 export default class GameSavingLoader {
   static async load() {
     const data = await read();
-    const value = await json(data);
-    return JSON.parse(value);
+    const parsedData = JSON.parse(await json(data));
+    const userInfo = new UserInfo(
+        parsedData.userInfo.id,
+        parsedData.userInfo.name,
+        parsedData.userInfo.level,
+        parsedData.userInfo.points
+    );
+    return new GameSaving(parsedData.id, parsedData.created, userInfo);
   }
 }
+
